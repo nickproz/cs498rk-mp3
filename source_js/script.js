5a2f9967b987ff644924f84342bfd52583939152
nick.proz <nick.prozorovsky@gmail.com>
2016-03-08 03:52:02 +0000

    added mp3 vars/mixins

--- source_js/script.js
+++ source_js/script.js
@@ -1,11 +1,4 @@
 // Write any custom javascript functions here
 $(document).ready(function() {
-	
-	$('.buttons').click(function() {
-		console.log('hello!');
-		$(this).toggleClass("active");
-		
-		
-	});
 
 });
